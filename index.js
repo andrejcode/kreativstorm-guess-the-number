10573788315011cc74cf0cd0e3ea470cc81a24f4
Andrej Milanović <amilanovi0@gmail.com>
2024-11-08 12:32:07 +0100

    Remove comments

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 const MAX_ATTEMPTS = 10;
 
-/**
- * Generates a random number between 1 and 100 (inclusive).
- *
- * @returns {number} A random number between 1 and 100.
- */
 function generateRandomNumber() {
   return Math.floor(Math.random() * 100) + 1;
 }
 
-/**
- * Prompts the user to enter a guess between 1 and 100.
- * Repeats the prompt until a valid number is entered.
- *
- * @returns {number|null} The player's guess if it's a valid number between 1 and 100, or null if the prompt is cancelled.
- */
 function getPlayerGuess() {
   while (true) {
     const input = prompt('Enter your guess (between 1 and 100):');
@@ -38,15 +27,6 @@ function getPlayerGuess() {
   }
 }
 
-/**
- * Checks the player's guess against the correct number.
- *
- * @param {number} playerGuess - The player's guess.
- * @param {number} correctNumber - The correct number to guess.
- * @returns {string} - A message indicating if the guess is "too low", "too high", or "correct".
- *
- * @throws {Error} - Throws an error if inputs are not valid numbers.
- */
 function checkGuess(playerGuess, correctNumber) {
   if (
     typeof playerGuess !== 'number' ||
@@ -68,12 +48,6 @@ function checkGuess(playerGuess, correctNumber) {
   }
 }
 
-/**
- * Generates a congratulatory message based on the number of attempts taken to guess the number.
- *
- * @param {number} attemptCounter - The number of attempts taken to guess the number.
- * @returns {string} A congratulatory message corresponding to the number of attempts.
- */
 function generateCongratulatoryMessage(attemptCounter) {
   switch (attemptCounter) {
     case 1:
@@ -87,13 +61,6 @@ function generateCongratulatoryMessage(attemptCounter) {
   }
 }
 
-/**
- * Runs the number guessing game.
- * The player has 10 attempts to guess the correct number.
- * After each guess, the player is informed whether their guess is too high, too low, or correct.
- * If the player guesses the correct number within 10 attempts, a congratulatory message is displayed.
- * If the player does not guess the correct number within 10 attempts, the correct number is revealed.
- */
 function game() {
   const correctNumber = generateRandomNumber();
   let attemptCounter = 1;
@@ -120,7 +87,7 @@ function game() {
         alert(
           `Your guess is ${checkGuessMessage}. Please try again!.\nYou have ${
             MAX_ATTEMPTS - attemptCounter
-          } ${attemptCounter === 9 ? 'attempt' : 'attempts'} left.` // Pluralize "attempt" if there is more than one attempt left.
+          } ${attemptCounter === 9 ? 'attempt' : 'attempts'} left.`
         );
       }
     } catch (error) {
